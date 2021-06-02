e4f67eb2c3e5fa27715c7bb05c1ef3a739e7b9f2
zenkoveric <zenkoveric@icloud.com>
2021-06-02 13:50:01 +1000

    Edge grabbing
    Added edge grabbing. Need improvement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -27,6 +27,12 @@ public class PlayerMovement : NetworkBehaviour
     public float maxSlopeAngle = 35f; 
     private Vector3 normalVector = Vector3.up;
 
+    //Grabbing
+    private bool readyToGrab = true;
+    private float grabCooldown = 1f;
+    public float grabForce = 250f;
+    public float grabRadius = 2f;
+
     //Input
     MyInput input = new MyInput();
     bool jumping;
@@ -139,11 +145,6 @@ public class PlayerMovement : NetworkBehaviour
         }
     }
 
-    void Grab()
-    {
-
-    }
-
     void ResetJump()
     {
         readyToJump = true;
@@ -186,5 +187,96 @@ public class PlayerMovement : NetworkBehaviour
     void StopGrounded()
     {
         grounded = false;
+    }
+
+    void Grab()
+    {
+        //Load points into a list
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, grabRadius, whatIsGround);
+        List<Vector2> points = new List<Vector2>();
+
+        bool hasPoints = false;
+        foreach(Collider2D collider in colliders)
+        {
+            SpriteRenderer renderer = collider.gameObject.GetComponent<SpriteRenderer>();
+            if(renderer)
+            {
+                Vector2[] corners = GetSpriteUpperCorners(renderer);
+                points.Add(corners[0]);
+                points.Add(corners[1]);
+                hasPoints = true;
+            }
+        }
+
+        //Find nearest point
+        Vector2 grabPoint = GetClosestPoint(points.ToArray());
+
+        //Apply forces
+        Vector2 legPos = (Vector2)transform.position + transform.localScale.y * Vector2.down;
+        Vector2 headPos = (Vector2)transform.position + transform.localScale.y * Vector2.up;
+        Vector2 distance = grabPoint - legPos;
+
+        float verticalForce = distance.y > 0 ? 8*distance.y/Mathf.Pow(0.1f, 2) : 0;
+        float horizontalForce = 2*distance.x/Mathf.Pow(0.1f, 2);
+        Vector2 vel = rb.velocity;
+        Vector2 force = new Vector2(horizontalForce, verticalForce) * 1.5f;
+        
+        //Recall if the point isn't reachable
+        RaycastHit2D bottomHit = Physics2D.Raycast(legPos, Vector2.down, grabRadius, whatIsGround);
+        RaycastHit2D ceilingHit = Physics2D.Raycast(headPos, Vector2.up, grabRadius, whatIsGround);
+        bool stopCondition = Vector2.Distance(transform.position, grabPoint) > grabRadius 
+        || !hasPoints
+        || bottomHit
+        || ceilingHit
+        || (Mathf.Sign(input.x) != Mathf.Sign(distance.x) && input.x != 0);
+
+        if(stopCondition) return;
+        rb.velocity = new Vector2(vel.x * 0.5f, 0);
+        rb.AddForce(force);
+
+        readyToGrab = false;
+        readyToJump = false;
+        Invoke(nameof(ResetGrab), grabCooldown);
+        Invoke(nameof(ResetJump), jumpCooldown);
+    }
+
+    void ResetGrab()
+    {
+        readyToGrab = true;
+    }
+
+    Vector2[] GetSpriteUpperCorners(SpriteRenderer renderer)
+    {
+        Vector2 topRight = renderer.transform.TransformPoint(renderer.sprite.bounds.max);
+        Vector2 topLeft = renderer.transform.TransformPoint(new Vector2(renderer.sprite.bounds.min.x, renderer.sprite.bounds.max.y));
+        return new Vector2[]{topRight, topLeft};
+    }
+
+    Vector2 GetClosestPoint(Vector2[] points)
+    {
+        Vector2 closest = new Vector2();
+        float minDist = Mathf.Infinity;
+        Vector2 currentPos = transform.position;
+
+        foreach(Vector2 point in points)
+        {
+            float dist = Vector2.Distance(currentPos, point);
+            if(dist < minDist)
+            {
+                closest = point;
+                minDist = dist;
+            }
+        }
+
+        return closest;
+    }
+
+    void OnDrawGizmos()
+    {
+        //Grabbing
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, grabRadius);
+        Gizmos.DrawSphere(transform.localScale.z*Vector2.down+(Vector2)transform.position, 0.1f);
+
     }
 }
